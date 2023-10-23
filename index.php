c1719f485bdf8d8eee21542fd1ddbb6171a52304
tridarma6 <triidarma06@gmail.com>
2023-10-23 17:55:33 +0800

    connecting to database local

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<h1>HELLO WORLD</h1>
-<h2>Pelajarin CRUD PHP ges</h2>
+<?php
+
+    $host       = "127.0.0.1:3308";
+    $username   = "root";
+    $password   = "";
+    $db         = "toolti";
+    
+    $connection = new mysqli($host, $username, $password, $db);
+    if($connection->connect_error){
+        die("Koneksi gagal").$connection->connect_error;
+    }else{
+        echo "Koneksi Berhasil";
+    }
+?>
