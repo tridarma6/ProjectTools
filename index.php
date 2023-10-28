f955ade61355d63e6827f54daa2451c33e587106
tridarma6 <triidarma06@gmail.com>
2023-10-28 16:53:27 +0800

    testing

--- index.php
+++ index.php
@@ -4,11 +4,27 @@
     $username   = "root";
     $password   = "";
     $db         = "toolti";
-    
+
     $connection = new mysqli($host, $username, $password, $db);
     if($connection->connect_error){
         die("Koneksi gagal").$connection->connect_error;
     }else{
         echo "Koneksi Berhasil";
     }
+
+    $id_customer    = 0;
+    $name           = "";
+    $alamat         = "";
+
+    $query = "SELECT * FROM customer";
+    $result = $connection->query($query);
+
+    foreach($result as $row){
+        $id_customer = $row['id_customer'];
+        $name        = $row['name'];
+        $alamat      = $row['alamat'];
+        echo "<br>".$id_customer." ".$name." ".$alamat;
+        echo "Testing";
+    }
+
 ?>
